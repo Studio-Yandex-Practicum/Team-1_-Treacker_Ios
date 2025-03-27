0e70cee62d805767fdc001adece1c6513fefc557
Konstantin Lyashenko <lyashenkokv@gmail.com>
2025-03-27 10:50:26 +0300

    feat: add firebase

--- SpendWise/AppDelegate.swift
+++ SpendWise/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
 
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     // MARK: - Core Data stack
