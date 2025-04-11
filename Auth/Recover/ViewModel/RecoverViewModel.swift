4b359598bba8ba81e34f425729a9b616a13441f7
Konstantin Lyashenko <lyashenkokv@gmail.com>
2025-04-11 18:02:06 +0300

    feat(SW-17): Добавить RecoverVC

---
+++ Auth/Recover/ViewModel/RecoverViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  RecoverViewModel.swift
+//  Auth
+//
+//  Created by Konstantin Lyashenko on 11.04.2025.
+//
+
+import Foundation
+import Combine
+import Core
+
+public final class RecoverViewModel {
+    @Published var email = ""
+    @Published private(set) var state: AuthState = .idle
+
+    private let authServices: EmailAuthService
+    private var cancellable = Set<AnyCancellable>()
+
+    public init(authServices: EmailAuthService = .init()) {
+        self.authServices = authServices
+    }
+
+    public func recover() {
+        state = .loading
+
+        authServices.resetPassword(email: email)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                guard let self else { return }
+                switch completion {
+                case .failure(let error):
+                    Logger.shared.log(
+                        .error,
+                        message: "Error recover password",
+                        metadata: ["❗️\(self)": "\(error.localizedDescription)"]
+                    )
+                    self.state = .failure(error)
+                case .finished:
+                    break
+                }
+            } receiveValue: { [weak self] in
+                self?.state = .success
+            }
+            .store(in: &cancellable)
+    }
+}
